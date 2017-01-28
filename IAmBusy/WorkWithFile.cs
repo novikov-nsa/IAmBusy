84ee77296d9945cf594c7ca28a584c675ceadb3c
snovikov <novikov.nsa@live.com>
2017-01-28 12:50:32 +0300

    описал создание файла, запись текста в файл

--- IAmBusy/WorkWithFile.cs
+++ IAmBusy/WorkWithFile.cs
@@ -5,26 +5,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using System.IO;
 
 namespace IAmBusy
 {
     public partial class WorkWithFile
     {
-        public string filename = null;
+        public string filename = "messageText.dat";
+        public const string defaultText = "Я на данный момент занят, перезвоню позже";
+        public string messageText = null;
         public StorageFolder storageFolder = null;
         public StorageFile storageFileName = null;
-
+        public StorageFile dataFile = null;
 
         public async void CheckFileIsExist() //Проверка существует ли файл
         {
-
+            try
+            {
+                storageFolder = ApplicationData.Current.LocalFolder;
+                dataFile = await storageFolder.GetFileAsync(filename);
+                Debug.WriteLine("Файл существует. Имя файла " + filename);
+            }
+            catch (FileNotFoundException)
+            {
+                Debug.WriteLine("Файл не существует");
+                CreateNewFile();
+                messageText = defaultText;
+                WriteToConfigFile();
+            }
         }
 
         public async void CreateNewFile() //Создание нового файла
         {
+            Debug.WriteLine("Создание файла");
             storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
             storageFileName = await storageFolder.CreateFileAsync(filename, Windows.Storage.CreationCollisionOption.ReplaceExisting);
-
+            Debug.WriteLine("Файл создан " + filename);
         }
 
 
@@ -36,9 +52,9 @@ namespace IAmBusy
             //чтение параметров из конфигюфайла
         }
 
-        public async void WriteToConfigFile()
+        public async void WriteToConfigFile(string messageText) //запись измененных параметров в конфиг.файл
         {
-            //запись измененных параметров в конфиг.файл
+            await Windows.Storage.FileIO.WriteTextAsync(storageFileName, messageText);
         }
 
 
